d14381afb09ecfd3389286baa982749af2acafba
pratham2021 <87680675+pratham2021@users.noreply.github.com>
2022-10-25 08:14:30 -0700

    push

--- Mario Game/index.js
+++ Mario Game/index.js
@@ -1,102 +1,19 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
-
-const gravity = 1.5
-
-class Player {
-  constructor() {
-    this.position = {
-      x: 100,
-      y: 100
-    }
-    this.velocity = {
-      x: 0,
-      y: 0
-    }
-
-    this.width = 30
-    this.height = 30
-  }
-
-  draw() {
-    c.fillStyle = 'red'
-    c.fillRect(this.position.x, this.position.y, this.width, this.height)
-  }
-
-  update() {
-    this.draw()
-    this.position.x += this.velocity.x
-    this.position.y += this.velocity.y
-
-    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
-      this.velocity.y += gravity
-    }
-    else {
-      this.velocity.y = 0
-    }
-  }
-}
+canvas.width = 64 * 16 // 1024
+canvas.height = 64 * 9 // 576
 
 const player = new Player()
-const keys = {
-  right: {
-    pressed: false
-  },
-  left: {
-    pressed: false
-  }
-}
 
+// let bottom = y + 100
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
-  player.update()
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'white'
+  c.fillRect(0, 0, canvas.width, canvas.height)
 
-  if (keys.right.pressed) {
-    player.velocity.x = 5
-  }
-  else if (keys.left.pressed) {
-    player.velocity.x = -5
-  }
-  else {
-    player.velocity.x = 0
-  }
+  player.draw()
+  player.update()
 }
 
 animate()
-
-addEventListener('keydown', ({ keyCode }) => {
-  switch (keyCode) {
-    case 65: // left
-      keys.left.pressed = true
-      break
-    case 83: // down
-      break
-    case 68: // right
-      console.log('right')
-      keys.right.pressed = true
-      break
-    case 87: // up
-      player.velocity.y -= 20
-      break
-  }
-})
-
-addEventListener('keyup', ({ keyCode }) => {
-  switch (keyCode) {
-    case 65: // left
-      keys.left.pressed = false
-      break
-    case 83: // down
-      break
-    case 68: // right
-      keys.right.pressed = false
-      break
-    case 87: // up
-      player.velocity.y -= 20
-      break
-  }
-})
