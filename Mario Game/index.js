77d4c3481aa9609532309f84d53222868e8f9f06
pratham2021 <prathamhebbar2021@gmail.com>
2022-11-11 17:25:06 -0800

    push

--- Mario Game/index.js
+++ Mario Game/index.js
@@ -8,6 +8,59 @@ let parsedCollisions
 let collisionBlocks
 let background
 let doors
+const player = new Player({
+  imageSrc: './img/king/idle.png',
+  frameRate: 11,
+  animations: {
+    idleRight: {
+      frameRate: 11,
+      frameBuffer: 2,
+      loop: true,
+      imageSrc: './img/king/idle.png',
+    },
+    idleLeft: {
+      frameRate: 11,
+      frameBuffer: 2,
+      loop: true,
+      imageSrc: './img/king/idleLeft.png',
+    },
+    runRight: {
+      frameRate: 8,
+      frameBuffer: 4,
+      loop: true,
+      imageSrc: './img/king/runRight.png',
+    },
+    runLeft: {
+      frameRate: 8,
+      frameBuffer: 4,
+      loop: true,
+      imageSrc: './img/king/runLeft.png',
+    },
+    enterDoor: {
+      frameRate: 8,
+      frameBuffer: 4,
+      loop: false,
+      imageSrc: './img/king/enterDoor.png',
+      onComplete: () => {
+        console.log('completed animation')
+        gsap.to(overlay, {
+          opacity: 1,
+          onComplete: () => {
+            level++
+
+            if (level === 4) level = 1
+            levels[level].init()
+            player.switchSprite('idleRight')
+            player.preventInput = false
+            gsap.to(overlay, {
+              opacity: 0,
+            })
+          },
+        })
+      },
+    },
+  },
+})
 
 let level = 1
 let levels = {
@@ -15,75 +68,98 @@ let levels = {
     init: () => {
       parsedCollisions = collisionsLevel1.parse2D()
       collisionBlocks = parsedCollisions.createObjectsFrom2D()
+      player.collisionBlocks = collisionBlocks
+      if (player.currentAnimation) player.currentAnimation.isActive = false
 
       background = new Sprite({
         position: {
           x: 0,
           y: 0,
         },
-        imageSrc: 'img/backgroundLevel1.png',
+        imageSrc: './img/backgroundLevel1.png',
       })
 
       doors = [
         new Sprite({
           position: {
-            x: 805,
-            y: 270
+            x: 767,
+            y: 270,
           },
           imageSrc: './img/doorOpen.png',
           frameRate: 5,
           frameBuffer: 5,
           loop: false,
-          autoplay: false
-        })
+          autoplay: false,
+        }),
+      ]
+    },
+  },
+  2: {
+    init: () => {
+      parsedCollisions = collisionsLevel2.parse2D()
+      collisionBlocks = parsedCollisions.createObjectsFrom2D()
+      player.collisionBlocks = collisionBlocks
+      player.position.x = 96
+      player.position.y = 140
+
+      if (player.currentAnimation) player.currentAnimation.isActive = false
+
+      background = new Sprite({
+        position: {
+          x: 0,
+          y: 0,
+        },
+        imageSrc: './img/backgroundLevel2.png',
+      })
+
+      doors = [
+        new Sprite({
+          position: {
+            x: 772.0,
+            y: 336,
+          },
+          imageSrc: './img/doorOpen.png',
+          frameRate: 5,
+          frameBuffer: 5,
+          loop: false,
+          autoplay: false,
+        }),
+      ]
+    },
+  },
+  3: {
+    init: () => {
+      parsedCollisions = collisionsLevel3.parse2D()
+      collisionBlocks = parsedCollisions.createObjectsFrom2D()
+      player.collisionBlocks = collisionBlocks
+      player.position.x = 750
+      player.position.y = 230
+      if (player.currentAnimation) player.currentAnimation.isActive = false
+
+      background = new Sprite({
+        position: {
+          x: 0,
+          y: 0,
+        },
+        imageSrc: './img/backgroundLevel3.png',
+      })
+
+      doors = [
+        new Sprite({
+          position: {
+            x: 176.0,
+            y: 335,
+          },
+          imageSrc: './img/doorOpen.png',
+          frameRate: 5,
+          frameBuffer: 5,
+          loop: false,
+          autoplay: false,
+        }),
       ]
     },
   },
 }
-
-const player = new Player({
-  collisionBlocks,
-  imageSrc: 'img/king/idle.png',
-  frameRate: 11,
-  animations: {
-    idleRight: {
-      frameRate: 11,
-      frameBuffer: 2,
-      loop: true,
-      imageSrc: 'img/king/idle.png',
-    },
-    idleLeft: {
-      frameRate: 11,
-      frameBuffer: 2,
-      loop: true,
-      imageSrc: 'img/king/idleLeft.png',
-    },
-    runRight: {
-      frameRate: 8,
-      frameBuffer: 4,
-      loop: true,
-      imageSrc: 'img/king/runRight.png',
-    },
-    runLeft: {
-      frameRate: 8,
-      frameBuffer: 4,
-      loop: true,
-      imageSrc: 'img/king/runLeft.png',
-    }, 
-    enterDoor: {
-      frameRate: 8,
-      frameBuffer: 4,
-      loop: false, 
-      imageSrc: 'img/king/enterDoor.png',
-      onComplete: () => {
-        console.log('completed animation')
-        gsap.to(overlay, {
-          opacity: 1
-        })
-      },
-    }
-  }
-})
 
 const keys = {
   w: {
@@ -105,9 +181,6 @@ function animate() {
   window.requestAnimationFrame(animate)
 
   background.draw()
-  collisionBlocks.forEach(collisionBlock => {
-    collisionBlock.draw()
-  })
 
   doors.forEach((door) => {
     door.draw()
@@ -124,4 +197,5 @@ function animate() {
   c.restore()
 }
 
+levels[level].init()
 animate()
--- Mario Game/js/classes/CollisionBlock.js
+++ Mario Game/js/classes/CollisionBlock.js
@@ -1,12 +1,12 @@
 class CollisionBlock {
     constructor({ position }) {
-        this.position = position
-        this.width = 64
-        this.height = 64
+      this.position = position
+      this.width = 64
+      this.height = 64
     }
-
+  
     draw() {
-        c.fillStyle = 'rgba(255, 0, 0, 0.5)'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+      c.fillStyle = 'rgba(255, 0, 0, 0.5)'
+      c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 }
